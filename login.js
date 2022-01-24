c19b851dd5258ff58987d5682f23b40084925f41
Schwalbe262 <r1jae@naver.com>
2022-01-25 00:02:57 +0900

    #2 로그인 코드 추가

---
+++ login.js
@@ -0,0 +1,42 @@
+/*
+ * Created on Wed Feb 17 2021
+ *
+ * Copyright (c) storycraft. Licensed under the MIT Licence.
+ */
+
+const { AuthApiClient, ChatBuilder, KnownChatType, MentionContent, ReplyContent, TalkClient, KnownAuthStatusCode } = require('node-kakao');
+
+
+
+var start = 1;
+
+env = reqruie("config.json")
+
+
+const CLIENT = new TalkClient();
+
+const DEVICE_UUID = env.UUID
+const DEVICE_ID = env.ID
+const DEVICE_PW = env.PW
+const DEVICE_NAME = "device"
+
+
+async function main() {
+    const api = await AuthApiClient.create(DEVICE_NAME, DEVICE_UUID);
+    const loginRes = await api.login({
+        email: DEVICE_ID,
+        password: DEVICE_PW,
+        // this option force login even other devices are log on
+        forced: true,
+    })
+
+
+    if (!loginRes.success) throw new Error(`Web login failed with status: ${loginRes.status}`);
+    console.log(`Received access token: ${loginRes.result.accessToken}`)
+
+    const res = await CLIENT.login(loginRes.result)
+    if (!res.success) throw new Error(`Login failed with status: ${res.status}`)
+
+    console.log("Login success")
+}
+main().then()
