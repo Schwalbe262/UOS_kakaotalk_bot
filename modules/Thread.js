992ca8bc3627d30a610f1201c30ba974c552b4c2
Schwalbe262 <r1jae@naver.com>
2022-02-20 20:43:19 +0900

    #29 공대 학과공지 파싱 스레드 추가

--- modules/Thread.js
+++ modules/Thread.js
@@ -24,11 +24,25 @@ Thread.Thread_UOSP1 = async function Thread_UOSP1(){
 
 Thread.Thread_dept = async function Thread_dept(){
 
-    botChannel.sendChat("Thread_UOSP1 실행")
+    botChannel.sendChat("Thread_dept 실행")
 
     while(1){
         try{
             await UOSP.dept_parsing(test=false,channels=noticeChannel,headless=true,dept="ECE",link="https://www.uos.ac.kr/engineering/korNotice/allList.do?list_id=20013DA1&cate_id2=000010058&epTicket=INV",dept_name="전전컴")
+            await Thread.sleep(10000)
+            await UOSP.dept_parsing(test=false,channels=noticeChannel,headless=true,dept="ECE_g",link="https://www.uos.ac.kr/engineering/korNotice/allList.do?list_id=20016D8&cate_id2=000010058&epTicket=LOG#a",dept_name="전전컴대학원")
+            await Thread.sleep(10000)
+            await UOSP.dept_parsing(test=false,channels=noticeChannel,headless=true,dept="CHEM",link="https://www.uos.ac.kr/engineering/korNotice/allList.do?list_id=20013DA1&cate_id2=000010059&epTicket=LOG",dept_name="화학공학과")
+            await Thread.sleep(10000)
+            await UOSP.dept_parsing(test=false,channels=noticeChannel,headless=true,dept="MECH",link="https://www.uos.ac.kr/engineering/korNotice/allList.do?list_id=20013DA1&cate_id2=000010060&epTicket=LOG",dept_name="기계정보공학과")
+            await Thread.sleep(10000)
+            await UOSP.dept_parsing(test=false,channels=noticeChannel,headless=true,dept="MAT",link="https://www.uos.ac.kr/engineering/korNotice/allList.do?list_id=20013DA1&cate_id2=000010061&epTicket=LOG",dept_name="신소재공학과")
+            await Thread.sleep(10000)
+            await UOSP.dept_parsing(test=false,channels=noticeChannel,headless=true,dept="CVN",link="https://www.uos.ac.kr/engineering/korNotice/allList.do?list_id=20013DA1&cate_id2=000010382&epTicket=LOG",dept_name="토목공학과")
+            await Thread.sleep(10000)
+            await UOSP.dept_parsing(test=false,channels=noticeChannel,headless=true,dept="COM",link="https://www.uos.ac.kr/engineering/korNotice/allList.do?list_id=20013DA1&cate_id2=000010383&epTicket=LOG",dept_name="컴퓨터과학부")
+            await Thread.sleep(10000)
+            await UOSP.dept_parsing(test=false,channels=noticeChannel,headless=true,dept="AI",link="https://www.uos.ac.kr/engineering/korNotice/allList.do?list_id=20013DA1&cate_id2=000010085&epTicket=LOG",dept_name="인공지능학과")
             await Thread.sleep(10000)
         }
         catch(e){
